8cdfc617bb3c9cb6f40a8fa72691903076780ef1
Kyoli48 <likyo48@gmail.com>
2024-07-26 18:29:53 +0700

    1.10-1.11 fixed table formatting

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text}) => (<button onClick={onClick}>{text}</button>)
 
+const StatisticLine = ({ text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const Statistics = ({good, neutral, bad}) => {
   const total = good+neutral+bad
   const ave = (good - bad) / total
@@ -16,17 +25,17 @@ const Statistics = ({good, neutral, bad}) => {
   }
   return (
     <div>
-      good {good}
-      <br />
-      neutral {neutral}
-      <br />
-      bad {bad}
-      <br />
-      all {total}
-      <br />
-      average {ave}
-      <br />
-      positive {positive}%
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={ave} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
+      %
     </div>
   )
 }
