4f878b3785b7039f82a5ccd5d6bc2dd5e604d70c
Kyoli48 <likyo48@gmail.com>
2024-07-31 12:27:47 +0700

    2.12-2.13 added backend & personService

---
+++ part2/part2-notes-frontend-part2-3/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import axios from 'axios'
+import noteService from './services/notes'
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+
+  //fetch initial array of note objects from server w GET
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  // create new note object with 'newNote' string as content; POST to server; concat response object to notes; reset newNote to empty string
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  // event handler function for toggling importance
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>      
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+      <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form> 
+    </div>
+  )
+}
+
+export default App
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+import personService from './services/persons'
 
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
@@ -19,12 +15,27 @@ const App = () => {
   const handleNewName = (event) => setNewName(event.target.value)
   const handleNewNumber = (event) => setNewNumber(event.target.value)
 
+  useEffect( () => {
+    personService
+      .getAll()
+      .then(initialPersons => setPersons(initialPersons))
+  }, [] )
+
   const addName = (event) => {
     event.preventDefault()
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} already exists`)
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
+      personService
+        .create(personObject)
+        .then(returnedPerson => 
+          setPersons(persons.concat(returnedPerson)))
       setNewName('')
       setNewNumber('')
     }
