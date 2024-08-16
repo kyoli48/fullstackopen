d6085173f40ff7e8237afe91531458d8c149315b
Kyoli48 <likyo48@gmail.com>
2024-08-16 15:37:45 +0800

    3.2-3.6 backend CRUD functionality

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -33,6 +33,58 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
     const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people.</p>
+        <p>${new Date()}</p>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => Number(p.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({ 
+          error: 'name/number missing' 
+        })
+    }
+
+    if (persons.find(p => body.name.toLowerCase() === p.name.toLowerCase())) {
+        return res.status(400).json({ 
+            error: 'name already exists' 
+        })
+    }
+
+    const person = {
+        "id": generateId(),
+        "name": body.name,
+        "number": body.number
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
 })
 
 const PORT = 3001
