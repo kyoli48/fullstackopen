b396b5384a19817dc6813f2850e5561f87400b28
Kyoli48 <likyo48@gmail.com>
2024-08-17 14:25:02 +0800

    3.12 mongoose

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
-
 morgan.token('body', req => 
     req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
 app.use(cors())
 app.use(express.static('dist'))
 
